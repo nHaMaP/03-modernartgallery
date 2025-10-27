eb81a9edce0c452d040b04dc47265eaf92023ac8
Anton <loni.personal@outlook.es>
2025-10-28 00:40:27 +0100

    changed vite.config.js in order to create several pages in /dist

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { resolve } from 'path';
 
 export default defineConfig({
   base: "./",
@@ -7,6 +8,14 @@ export default defineConfig({
       usePolling: true, 
     },
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        location: resolve(__dirname, 'location.html'),  
+      }
+    }
+  }
 });
 
 
